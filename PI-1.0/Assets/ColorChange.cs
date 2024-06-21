4db60aaab52ffedbff893a4fd2cb8b1cc6973864
JoanaMlouri <jo.mlourinho@gmail.com>
2024-06-21 10:32:32 -0300

    primeiros códigos

---
+++ PI-1.0/Assets/ColorChange.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Defina as cores para alternar
+    public Color color1 = Color.red;
+    public Color color2 = Color.white;
+
+    private Renderer objectRenderer;
+    private bool isColor1 = true;
+
+    void Start()
+    {
+        // Obtém o Renderer do objeto
+        objectRenderer = GetComponent<Renderer>();
+        // Define a cor inicial
+        objectRenderer.material.color = color1;
+    }
+
+    void OnMouseDown()
+    {
+        // Alterna a cor do objeto quando for clicado
+        if (isColor1)
+        {
+            objectRenderer.material.color = color2;
+        }
+        else
+        {
+            objectRenderer.material.color = color1;
+        }
+
+        isColor1 = !isColor1;
+    }
+}
