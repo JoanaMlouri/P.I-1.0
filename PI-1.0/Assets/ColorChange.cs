9d76cee26d67ecd4a8fd3199b8604d58f1c96686
JoanaMlouri <jo.mlourinho@gmail.com>
2024-06-21 10:42:05 -0300

    scrip funcionando

--- PI-1.0/Assets/ColorChange.cs
+++ PI-1.0/Assets/ColorChange.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ColorChange : MonoBehaviour
 {
     // Defina as cores para alternar
     public Color color1 = Color.red;
