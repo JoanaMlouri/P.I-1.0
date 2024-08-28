216ed6c83b6e5cfb18855a32ca0fdfbd1b146cad
JoanaMlouri <jo.mlourinho@gmail.com>
2024-08-28 14:32:29 -0300

    Foi culpa do Cruz também

---
+++ PI-1.0/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Transform pauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.gameObject.activeSelf)
+            {
+                pauseMenu.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                pauseMenu.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+
+        }
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    internal static void SetActive(bool v)
+    {
+        throw new NotImplementedException();
+    }
+}
