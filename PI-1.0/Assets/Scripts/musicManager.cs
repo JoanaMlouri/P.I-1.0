109b406986222ae5846a869861146411eaf99587
JoanaMlouri <jo.mlourinho@gmail.com>
2024-11-19 13:59:39 -0300

    sla mah

--- PI-1.0/Assets/Scripts/musicManager.cs
+++ PI-1.0/Assets/Scripts/musicManager.cs
@@ -1,3 +1,21 @@
-﻿internal class MusicManager
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
 {
+    private static MusicManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Faz o objeto persistir
+        }
+        else
+        {
+            Destroy(gameObject); // Remove duplicatas
+        }
+    }
 }
